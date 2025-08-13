a5e109f04202a191550199f6d2955e6ac5c5d0ec
zxy_tencent_linux <zxy_tencent_linux>
2025-08-13 21:18:08 +0800

    删除登陆界面

---
+++ low_storage/2.零钱兑换.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int coinChange(vector<int> &coins, int amount)
+    {
+        vector<int> dp (amount+1 ,INT_MAX);
+        //初始化
+        dp[0] = 1;
+        for(int i = 0 ; i <coins.size() ; i++)
+        {
+            for(int j = coins[i] ; j <= amount ; j ++)
+            {
+                dp[j] = min(dp[j],dp[j-coins[i]] +1);
+            }
+        }
+        return dp[amount];
+    }
+};
